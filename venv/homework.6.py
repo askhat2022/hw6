3eddbc8230de2ea34c9585e94ad1d74d74d5dc74
Bayaliev <askad634@gmail.com>
2022-01-24 16:42:45 +0600

    first commit

--- venv/homework.6.py
+++ venv/homework.6.py
@@ -1,10 +1,9 @@
-num = int(input("Insert the number: "))
-
-
+num = int(input('Insert the number:'))
 class Summa:
-    def __init__(self, numbers: list, disared_sum: int):
+    def __init__(self, numbers: list, desired_sum: int):
         self.numbers = numbers
-        self.disared_sum = disared_sum
+        self.desired_sum = desired_sum
+
 
     def index(self):
         for i in range(len(self.numbers)):
@@ -13,6 +12,6 @@ class Summa:
                     return (i, n)
 
 
-num_1 = Summa(numbers=[2, 7, 11, 15], disared_sum=num)
-print(num_1)
+num_1 = Summa(numbers=[2, 7, 11, 15], desired_sum=num)
+print(num)
 print(num_1.index())
